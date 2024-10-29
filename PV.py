940e35326f3148be1bcc333afe3862fb4db0f723
cam-eldridge-99 <cameron.a.eldridge@gmail.com>
2024-10-29 15:05:10 +0000

    Update PV.py

--- PV.py
+++ PV.py
@@ -5,18 +5,18 @@ Spyder Editor
 This is a temporary script file.
 """
 
-import mpmath
+import math
 
 def find_roof_area():
-    building_width = int(input("How wide is your building in meters?"))
-    building_length = int(input("How long is your building in meters?"))
-    roof_angle= int(input("What is your roof angle in degrees?"))*(3.14/180)
+    building_width = int(input("How wide is your building in meters?\n"))
+    building_length = int(input("How long is your building in meters?\n"))
+    roof_angle= int(input("What is your roof angle in degrees?\n"))*(3.14/180)
     #math.cos(math.radians(roof_angle))
-    roof_area = building_width * building_length * sec(roof_angle)
-    print("Your available roof area is {} meters.".format(roof_area))
+    roof_area = building_width * building_length / math.cos(roof_angle)
+    print("\n Your available roof area is {:.2f} meters.".format(roof_area))
     return roof_area;
     
-find_roof_area()
+roof_area=find_roof_area()
 
 class pvPanel():
     def __init__(self, pv_width, pv_height, pv_power):
@@ -24,10 +24,16 @@ class pvPanel():
         self.pv_height = pv_height
         self.pv_power = pv_power
         
-    def pv_area(self):
-        pv_area = (self.pv_width/1000) * (self.pv_height/1000)
-        return pv_area;
+def pv_area(self):
+    pv_area = (self.pv_width/1000) * (self.pv_height/1000)
+    return pv_area;
 
 ourPanel = pvPanel( 1690, 1046, 400)
 
-print("Each panel is {:.2f} square meters.".format(pv_area))
+panelArea = pv_area(ourPanel)
+
+numPanels = math.floor(roof_area / panelArea)
+
+print("Each panel is {:.2f} square meters.\n".format(panelArea))
+
+print("The roof will fit {} panels.".format(numPanels))
