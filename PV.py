8cc44c6c3d77db2880ed09f6dca85bffd55db24e
cam-eldridge-99 <cameron.a.eldridge@gmail.com>
2024-10-29 15:26:09 +0000

    Update PV.py

--- PV.py
+++ PV.py
@@ -11,12 +11,11 @@ def find_roof_area():
     building_width = int(input("How wide is your building in meters?\n"))
     building_length = int(input("How long is your building in meters?\n"))
     roof_angle= int(input("What is your roof angle in degrees?\n"))*(3.14/180)
-    #math.cos(math.radians(roof_angle))
+    #math.cos(math.radians(roof_angle) would probably be faster
     roof_area = building_width * building_length / math.cos(roof_angle)
-    print("\n Your available roof area is {:.2f} meters.".format(roof_area))
+    print("\nYour available roof area is {:.2f} meters.".format(roof_area))
     return roof_area;
     
-roof_area=find_roof_area()
 
 class pvPanel():
     def __init__(self, pv_width, pv_height, pv_power):
@@ -28,6 +27,8 @@ def pv_area(self):
     pv_area = (self.pv_width/1000) * (self.pv_height/1000)
     return pv_area;
 
+roof_area=find_roof_area()
+
 ourPanel = pvPanel( 1690, 1046, 400)
 
 panelArea = pv_area(ourPanel)
@@ -36,6 +37,6 @@ numPanels = math.floor(roof_area / panelArea)
 
 powerDelivered = ourPanel.pv_power * numPanels / 1000
 
-print("Each panel is {:.2f} square meters.\n".format(panelArea))
+print("Each panel is {:.2f} square meters.".format(panelArea))
 
-print("The roof will fit {} panels and deliver {:.2f} kw.".format(numPanels, powerDelivered))
+print("The roof will fit {} panels and deliver {:.2f} kw.\n".format(numPanels, powerDelivered))
